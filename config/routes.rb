a25413794a52a17672e00b0a1f67c3493fe5ccb5
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-10-21 23:56:23 +1100

    Add integration test for users

--- config/routes.rb
+++ config/routes.rb
@@ -274,6 +274,9 @@ Rails.application.routes.draw do
 
       get "users", to: "users#edit"
       patch "users", to: "users#update"
+
+      get "confirm", to: "confirm#show"
+      patch "confirm", to: "confirm#update"
     end
   end
 
